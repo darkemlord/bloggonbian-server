9254eeebab860c0bb5398fea71a2e2e6e5cc3c3e
emanuel caro <darkemlord@gmail.com>
2024-03-23 14:11:26 +0900

    refs #1 fixing migration

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_23_045804) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
