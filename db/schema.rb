f5b636993ef8786668e68d9f45775af0ccbc2359
emanuel caro <darkemlord@gmail.com>
2024-04-07 19:03:54 +0900

    refs #1 installing annotate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_23_045804) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_24_030124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "string"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.bigint "genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genre_id"], name: "index_posts_on_genre_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_23_045804) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "genres"
+  add_foreign_key "posts", "users"
 end
