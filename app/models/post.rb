ec3218a73ce6102ae4efd87d2f516e00b52a109a
emanuel caro <darkemlord@gmail.com>
2024-04-07 19:08:13 +0900

    refs #1 adding annotation in all models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  string     :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  genre_id   :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_genre_id  (genre_id)
+#  index_posts_on_user_id   (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (genre_id => genres.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :genre
