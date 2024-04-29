d5ba3ec3d3e365bc29eb0d4cc56963aef12144cb
emanuel caro <darkemlord@gmail.com>
2024-04-29 21:31:28 +0900

    refs #1 fixing  quotes and indentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,26 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-put "Starting seeding ...."
+put 'Starting seeding ....'
 
 10.times do |i|
   User.create!(
     name: "user #{i + 1}",
     email: "user#{i + 1}@ejemplo.com",
-    password: 'password123',
+    password: 'password123'
   )
 end
 
 genres = [
   { name: 'Ciencia Ficción', description: 'Exploración de conceptos futurísticos, ciencia y tecnología.' },
-  { name: 'Fantasía', description: 'Elementos mágicos y sobrenaturales como parte central de la trama, el tema o el escenario.' },
-  { name: 'No Ficción', description: 'Escritos basados en hechos, eventos reales y personas reales, como biografías o historias.' }
+  {
+    name: 'Fantasía',
+    description: 'Elementos mágicos y sobrenaturales como parte central de la trama, el tema o el escenario.'
+  },
+  {
+    name: 'No Ficción',
+    description: 'Escritos basados en hechos, eventos reales y personas reales, como biografías o historias.'
+  }
 ]
 
 genres.each do |genre|
@@ -29,7 +35,8 @@ end
 10.times do |i|
   post = Post.new(
     title: "Post Título #{i + 1}",
-    body: "Este es el cuerpo del post #{i + 1}. Es simplemente un texto de relleno para ilustrar la creación de múltiples posts."
+    body: "Este es el cuerpo del post #{i + 1}.
+      Es simplemente un texto de relleno para ilustrar la creación de múltiples posts."
   )
   users = User.all
   genres = Genre.all
