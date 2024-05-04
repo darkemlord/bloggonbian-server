0baa782d5cfe2901b92abdfae22df573e716da99
emanuel caro <darkemlord@gmail.com>
2024-05-04 22:54:55 +0900

    refs #1 fixing routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+# Routes initializer
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1, defaults: { format: :json }  do
+    namespace :v1, defaults: { format: :json } do
       resources :users, :genres, :posts
     end
   end
